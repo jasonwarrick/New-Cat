3fced547958bd73b261cb02971b16e04a75e97c8
jasonwarrick <100223827+jasonwarrick@users.noreply.github.com>
2024-08-30 17:19:00 -0400

    Queue for cat destinations

--- New Cat/Assets/Cat/SC_CatDestinationSetter.cs
+++ New Cat/Assets/Cat/SC_CatDestinationSetter.cs
@@ -1,12 +1,30 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MoveTo : MonoBehaviour {
     
     [SerializeField] Transform goal;
+
+    bool readyToGo = false;
+
+    Queue<Transform> destinations = new Queue<Transform>();
+
+    NavMeshAgent agent;
     
-    void Start () {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position; 
+    void Awake () {
+        agent = GetComponent<NavMeshAgent>();
+        AddDestination(goal);
+        readyToGo = true;
+    }
+
+    public void AddDestination(Transform newDestination) {
+        destinations.Enqueue(newDestination);
+    }
+
+    void Update() {
+        if (readyToGo && destinations.Count > 0) {
+            agent.SetDestination(destinations.Dequeue().position);
+        }
     }
 }
